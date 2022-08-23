9a64191366010afcce2641feccf3d9dfc9eaaf26
bhaumikkpatel <bhaumikkpatel11@gmail.com>
2022-08-23 20:32:54 +0530

    practical_1_1

---
+++ src/main/kotlin/practical_1_1.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var a: Int = 11
+    var b: Long = 15565
+    var c: Byte= 64
+    var d: Short= 1483
+    var e: Char ='b'
+    var f: String = "bhaumikpatel"
+    var x:Float=14.33f
+    var y:Double=15.380
+    var z:Boolean=false
+    println(a)
+    println(b)
+    println(c)
+    println(d)
+    println(e)
+    println(f)
+    println(x)
+    println(y)
+    println(z)
+}
