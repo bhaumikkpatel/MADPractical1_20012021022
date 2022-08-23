254a3b8c54a4b461c65ce7a7103c101f61cee761
bhaumikkpatel <bhaumikkpatel11@gmail.com>
2022-08-23 20:50:40 +0530

    practical_1_1

---
+++ src/main/kotlin/practical_1_2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val a: Int=11
+    val b:Double= a.toDouble()
+    println("Int to double")
+    println(b)
+    val c: String ="45"
+    val d:Int= c.toInt()
+    println("string to Int")
+    println(d)
+    val e: String="16"
+    val f:Double= e.toDouble()
+    println("string to double")
+    println(f)
+}
